931dc4bf019f6a467a7cd5094e3522904d9016ab
Mahinur26 <mahinurmahi26@gmail.com>
2025-06-29 01:49:52 -0400

    Working on movement of the tiles. Added most of the lambda functions needed if the tiles move  left, need to add the feature for the rest of the directions.

--- 2048.py
+++ 2048.py
@@ -74,14 +74,26 @@ class Tile:
         #Text is drawn on the rectangle and centered
         window.blit(text, (self.x + (RECT_WIDTH / 2 - text.get_width() / 2), self.y + (RECT_HEIGHT / 2 - text.get_height() / 2)))
 
-    def set_position(self, row, col):
-        self.row = row
-        self.col = col
-        self.x = GRID_X + col * RECT_WIDTH
-        self.y = GRID_Y + row * RECT_HEIGHT
+
+
+#LOOK BACK AT THIS
+#LOOK BACK AT THIS
+#LOOK BACK AT THIS
+#LOOK BACK AT THIS
+#LOOK BACK AT THIS
+    def set_position(self, ceil=False):
+        if ceil:
+            #Rounds up the value 
+            self.row = math.ceil(self.y / RECT_HEIGHT) 
+            self.row = math.ceil(self.x / RECT_WIDTH)
+        else:
+            #Rounds down the value
+            self.row = math.floor(self.y / RECT_HEIGHT) 
+            self.row = math.floor(self.x / RECT_WIDTH)
 
     def move(self,delta):
-        pass
+        self.x += delta[0]
+        self.y += delta[1]
 
 #Drawing the grid and background
 def draw_grid(window):
@@ -132,6 +144,89 @@ def generate_tiles():
         tiles[f"{row}{col}"] = Tile(2, row, col)
 
     return tiles
+
+
+#This function will handle the movement of the tiles in the grid
+#The direction parameter will be used to determine the direction of the movement
+#The clock parameter is used to ensure that the movement is smooth and consistent
+def mov_tiles(window, tiles, clock, direction):
+    updated = True
+    #Tells which tiles have already merged
+    blocks = set()
+
+    if direction == "left":
+        sort_func = lambda x: x.col
+        reverse = False
+        delta = (-MOVE_VELOCITY,0)
+        #If col is 0, then it can't move left
+        boundary_check = lambda tile: tile.col == 0
+        get_next_tile = lambda tile: tiles.get(f"{tile.row}{tile.col - 1}")
+        #The move velocity will start at 20 and then go to 0, which will allow for the tile to merge when both tiles overlap
+        merge_check = lambda tile, next_tile: tile.x > next_tile.x + MOVE_VELOCITY
+        #Used to check if the tile can keep on moving to the left, and since the tile on the left has a different value,
+        #this lambda function will return False as soon as the border of the tile is reached
+        move_check = lambda tile, next_tile: tile.x > next_tile.x + RECT_WIDTH + MOVE_VELOCITY
+        ceil = True
+    elif direction == "right":
+        #If col is 3, then it can't move right
+        boundary_check = lambda tile: tile.col == 3
+        get_next_tile = lambda tile: tiles.get(f"{tile.row}{tile.col +1}")
+        pass
+    elif direction == "up":
+        pass
+    elif direction == "down":
+        pass
+
+    while updated:
+        clock.tick(FPS)
+        updated = False
+        #Tiles need to be sorted based on their position in the grid
+        #This is done so that the tiles can be moved in the correct order
+        sorted_tiles = sorted(tiles.values(), key=sort_func, reverse=reverse)
+        #Getting index and each Tile object in the sorted tiles
+        for i, tile in enumerate(sorted_tiles):
+            if boundary_check(tile):
+                continue
+            
+            #Gets the tile next to the current tile in the specified direction
+            next_tile = get_next_tile(tile)
+            #Continues to move the tile if it's not at the boundary and there isn't a next tile
+            if not next_tile:
+                tile.move(delta)
+
+            #Ensures the tiles are the same value to merge and makes sure the tiles are not already merged with another tile
+            elif (tile.value == next_tile.value) and (tile not in blocks) and (next_tile not in blocks):
+                #Tile keeps on moving until it overlaps the next tile of the same value
+                if merge_check(tile, next_tile):
+                    tile.move(delta)
+                else:
+                    #Simulates adding the two tiles together as they're always powers of 2
+                    next_tile.value *= 2
+                    #The current tile is removed and the next tile is added to the blocks set
+                    sorted_tiles.pop(i)
+                    blocks.add(next_tile)
+            #We know there is a next tile, but the values are different
+            #So the tile will keep on moving unyil it reaches the border of the next tile
+            elif move_check(tile, next_tile):
+                tile.move(delta)
+            #No possible move is possible so there wil be no update
+            else:
+                continue
+            #As long as no continue statement is reached, the window will be updated
+            tile.set_position(ceil)
+            updated = True
+        
+        update_tiles(window, tiles, sorted_tiles)
+
+#FINISH THIS FUNCTION
+def update_tiles(window, tiles, sorted_tiles):
+    pass
+    tile.clear()
+    for tile in sorted_tiles:
+        tiles[f"{tile.row}{tile.col}"] = Tile(2, row, col)       
+                
+
+
 
 #Main game loop
 def main(window):
